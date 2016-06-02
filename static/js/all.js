6f5d232408c6ecf3ef178b9372ebaffbcdbaf376
Brad Orego <me@bradorego.com>
2016-06-02 15:21:27 -0500

    cleaning up a bunch of unused CSS; giving photo credit link shadow; fixing positioning of photo link for large screen

--- static/js/all.js
+++ static/js/all.js
@@ -1,1 +1,60 @@
-Element.prototype.hasClass=function(e){for(var t=this.className.split(" "),s=0;s<t.length;s++)if(t[s]===e)return!0;return!1},Element.prototype.removeClass=function(e){return this.hasClass(e)?(this.className=this.className.replace(e,""),!0):!1},Element.prototype.addClass=function(e){return this.hasClass(e)?!1:(this.className+=" "+e,!0)},Element.prototype.toggleClass=function(e){this.hasClass(e)?this.removeClass(e):this.addClass(e)},document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("wrapper"),t=document.getElementsByClassName("menu-toggle")[0],s=document.getElementById("scrollUp");t.addEventListener("click",function(t){e.toggleClass("toggled")}),s&&(window.addEventListener("scroll",function(e){window.scrollY>300?s.removeClass("hidden"):s.addClass("hidden")}),s.addEventListener("click",function(e){window.scrollTo(0,0)}))});
+/*!
+ * Clean Blog v1.0.0 (http://startbootstrap.com)
+ * Copyright 2014 Start Bootstrap
+ * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
+ */
+
+Element.prototype.hasClass = function (name) {
+  var classes = this.className.split(" "),
+    hasClass = false;
+  for (var i = 0; i < classes.length; i++) {
+    if (classes[i] === name) {
+      return true;
+    }
+  }
+  return false;
+};
+Element.prototype.removeClass = function (name) {
+  if (this.hasClass(name)) {
+    this.className = this.className.replace(name, '');
+    return true;
+  } else {
+    return false;
+  }
+};
+Element.prototype.addClass = function (name) {
+  if (!this.hasClass(name)) {
+    this.className += ' ' + name;
+    return true;
+  } else {
+    return false;
+  }
+};
+Element.prototype.toggleClass = function (name) {
+  if (this.hasClass(name)) {
+    this.removeClass(name);
+  } else {
+    this.addClass(name);
+  }
+};
+document.addEventListener('DOMContentLoaded', function () {
+  var $wrapper = document.getElementById('wrapper'),
+    $menuToggle = document.getElementsByClassName('menu-toggle')[0],
+    $scrollUp = document.getElementById('scrollUp');
+  $menuToggle.addEventListener('click', function (e) {
+    $wrapper.toggleClass('toggled');
+  });
+
+  if ($scrollUp) {
+    window.addEventListener('scroll', function (e) {
+      if (window.scrollY > 300) {
+        $scrollUp.removeClass('hidden');
+      } else {
+        $scrollUp.addClass('hidden');
+      }
+    });
+    $scrollUp.addEventListener('click', function (e) {
+      window.scrollTo(0, 0);
+    });
+  }
+});
